85a5b9a10dbb37e9e8be14c530df70eb8d40f5ae
lincoln <lincolnhammed@gmail.com>
2026-09-18 15:49:30 +0100

    feat: implement task listing and update

--- todolist/src/main/java/pt/lincolnsilva/todolist/task/TaskController.java
+++ todolist/src/main/java/pt/lincolnsilva/todolist/task/TaskController.java
@@ -1,17 +1,17 @@
 package pt.lincolnsilva.todolist.task;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.context.SecurityContextHolder;
-import pt.lincolnsilva.todolist.user.IUserRepository;
+import pt.lincolnsilva.todolist.user.UserModel;
+import pt.lincolnsilva.todolist.util.Utils;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
+
 
 @RestController
 @RequestMapping("/tasks")
@@ -20,29 +20,123 @@ public class TaskController {
     @Autowired
     private ITaskRepository taskRepository;
 
-    @Autowired
-    private IUserRepository userRepository;
-
     @PostMapping("/")
-    public ResponseEntity create(@RequestBody TaskModel taskModel) {
+    public ResponseEntity<?> create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+
+        // Pega a autenticação atual
+        var authentication =
+                SecurityContextHolder
+                        .getContext()
+                        .getAuthentication();
+
+        /* Se não encontrar, tenta pelo Repository
+        if (authentication == null) {
+
+            var repository =
+                    new RequestAttributeSecurityContextRepository();
+
+            authentication =
+                    repository
+                            .loadDeferredContext(request)
+                            .get()
+                            .getAuthentication();
+        }
+
+        // Verifica se existe usuário autenticado
+        if (authentication == null) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body("Usuário não autenticado");
+        }
+        */
+        var user = (UserModel) authentication.getPrincipal();
+
+        taskModel.setIdUser(user.getId());
+
+        var currentDate = LocalDateTime.now();
+        if (taskModel.getStartAt() == null
+                || taskModel.getEndAt() == null) {
+
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("As datas são obrigatórias");
+        }
+
+        if (!taskModel.getStartAt().isAfter(currentDate)
+                || !taskModel.getEndAt().isAfter(currentDate)) {
+
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("As datas devem ser posteriores à data atual");
+        }
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início deve ser anterior à data de término");
+        }
+
+        var task = this.taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(task);
+    }
+
+    @GetMapping("/lista")
+    public ResponseEntity<?> list(HttpServletRequest request) {
 
         var authentication =
                 SecurityContextHolder
                         .getContext()
                         .getAuthentication();
 
-        String username = authentication.getName();
+        var user = (UserModel) authentication.getPrincipal();
 
-        var user = userRepository.findByUsername(username);
+        var tasks = this.taskRepository.findByIdUser(user.getId());
 
-        taskModel.setIdUser(user.getId());
+        if (tasks.isEmpty()) {
+            return ResponseEntity
+                    .status(HttpStatus.NO_CONTENT)
+                    .build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(tasks);
+    }
 
-        var currentDate = LocalDateTime.now();
-        if (currentDate.isAfter(taskModel.getStartAt())) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("a data deve ser maior que a data atual");
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+
+        var authentication =
+                SecurityContextHolder
+                        .getContext()
+                        .getAuthentication();
+
+        var user = (UserModel) authentication.getPrincipal();
+
+        var taskUp = this.taskRepository.findById(id).orElse(null);
+
+
+
+        if (taskUp == null) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)//404
+                    .body("Usuario nao existe");
+        }
+        if (!taskUp.getIdUser().equals(user.getId())) {
+            return ResponseEntity
+                    .status(HttpStatus.FORBIDDEN) // 403 - Proibido
+                    .body("Você não tem permissão para alterar esta tarefa.");
 
         }
-        var task = this.taskRepository.save(taskModel);
+        // System.out.println("Titulo recebido: " + taskModel.getTitulo());
+        // System.out.println("Titulo antigo: " + taskUp.getTitulo());
+
+        Utils.copyNonNullProperties(taskModel, taskUp);
+
+        // System.out.println("Titulo depois do copy: " + taskUp.getTitulo());
+        //taskUp.setTitulo(taskModel.getTitulo());
+        //taskUp.setDescricao(taskModel.getDescricao());
+
+        var task = this.taskRepository.save(taskUp);
         return ResponseEntity.status(HttpStatus.OK).body(task);
     }
+
 }
