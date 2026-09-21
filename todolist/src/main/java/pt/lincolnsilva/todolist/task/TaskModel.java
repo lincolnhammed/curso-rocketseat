fb12c88b90719f7654c5bb5133b997211bd80cbe
lincoln <lincolnhammed@gmail.com>
2026-09-21 17:18:57 +0100

    feat: add validation for task title length

--- todolist/src/main/java/pt/lincolnsilva/todolist/task/TaskModel.java
+++ todolist/src/main/java/pt/lincolnsilva/todolist/task/TaskModel.java
@@ -1,6 +1,7 @@
 package pt.lincolnsilva.todolist.task;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -16,6 +17,7 @@ public class TaskModel {
     private UUID id;
     private String descricao;
     @Column(length = 50)
+//    @Size(max = 50, message = "O título deve conter no máximo 50 caracteres")
     private String titulo;
     private LocalDateTime startAt;
     private LocalDateTime endAt;
@@ -26,8 +28,10 @@ public class TaskModel {
     @CreationTimestamp
     private LocalDateTime createdAt;
 
-
-
-
-
+    public void setTitulo(String titulo) throws Exception {
+        if (titulo != null && titulo.length() > 50) {
+            throw new Exception("o campo titulo deve conter no maximo 50 caracteres");
+        }
+        this.titulo = titulo;
+    }
 }
